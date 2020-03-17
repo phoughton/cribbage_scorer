b51bba3560f8ba886d141cc38e14d205b373ed38
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-03-17 14:23:08 +0000

    Added 15s and multiples.

--- cribbage_scorer.py
+++ cribbage_scorer.py
@@ -1,15 +1,70 @@
 from collections import defaultdict
+import itertools
+
 
 def score(starter, hand):
-    all_cards=[]
-    all_cards.append(starter)
-    all_cards.append(hand)
-    
-    
-    double_counter = defaultdict(lambda: 0)
+    running_score = 0
+    running_msg = ""
+
+    multiples_score, multiples_msg = multiples(starter, hand)
+    running_score += multiples_score
+    running_msg = running_msg + multiples_msg
+
+    fifteens_score, fifteens_msg = fifteens(starter, hand)
+    running_score += fifteens_score
+    running_msg = running_msg + fifteens_msg
+
+    return running_score, running_msg
+
+
+def fifteens(starter, hand):
+    all_cards = hand + [starter]
+    print("Combined hand: " + str(all_cards))
+
+    running_score = 0
+    score_msg = ""
+
+    combinations_2_cards = itertools.combinations(all_cards, 2)
+    for two_card_sequence in combinations_2_cards:
+        if two_card_sequence[0][0] + two_card_sequence[1][0] == 15:
+            running_score += 2
+            score_msg = score_msg + f"15 from {two_card_sequence}"
+
+    combinations_3_cards = itertools.combinations(all_cards, 3)
+    for three_card_sequence in combinations_3_cards:
+        if three_card_sequence[0][0] + three_card_sequence[1][0] + three_card_sequence[2][0] == 15:
+            running_score += 2
+            score_msg = score_msg + f"15 from {three_card_sequence}"
+
+    return running_score, score_msg
+
+
+def multiples(starter, hand):
+    all_cards = hand + [starter]
+    print("Combined hand: " + str(all_cards))
+
+    multiples_counter = defaultdict(lambda: 0)
     for card in all_cards:
-        double_counter[card[0]] = double_counter[card[0]]
-    
-    double_counter.keys
-    
-    return 1
+        multiples_counter[card[0]] += 1
+
+    multiples_found = {}
+    for card_name, count in multiples_counter.items():
+        if count > 1:
+            multiples_found[card_name] = count
+
+    score_total = 0
+    score_msg = ""
+    for card, count in multiples_found.items():
+        if count == 2:
+            score_total += 2
+            score_msg = score_msg + f"2 points for double {card}s, "
+        elif count == 3:
+            score_total += 6
+            score_msg = score_msg + f"6 points for triple {card}s, "
+        elif count == 4:
+            score_total += 12
+            score_msg = score_msg + f"12 points for quadruple {card}s, "
+        else:
+            raise Exception(f"An illegal multiple  of {count} was found in hand: {hand}")
+
+    return score_total, score_msg
--- scorer_tests.py
+++ scorer_tests.py
@@ -3,19 +3,35 @@ import cribbage_scorer
 
 
 @pytest.mark.parametrize("starter, hand, expected_score, description", [
-        ((5,"H"), [(7,"S"),(9,"S"), (2,"C"), (9,"H")], 2, "simple, double 9"),
-        ((4,"H"), [(4,"S"),(9,"S"), (1,"C"), (9,"H")], 2, "simple, double 4"),
-        ((11,"S"), [(12,"D"),(13,"S"), (1,"C"), (9,"H")], 3, "simple, 3 card run"),
-        ((1,"S"), [(5,"D"),(10,"S"), (2,"C"), (6,"H")], 2, "simple, 15"),
-        ((5,"H"), [(5,"S"),(4,"S"), (2,"S"), (6,"H")], 12, "mixed, 15s, double run"),
-        ((5,"H"), [(6,"D"),(11,"H"), (4,"H"), (7,"C")], 9, "mixed, 15s, run, nob"),
-        ((5,"H"), [(10,"S"),(8,"D"), (13,"C"), (8,"C")], 6, "mixed, 15s, pair")
+        ((5, "H"), [(7, "S"), (9, "S"), (2, "C"), (9, "H")], 2, "simple, double 9"),
+        ((4, "H"), [(4, "S"), (9, "S"), (1, "C"), (9, "H")], 4, "simple, double 4 and double 9"),
+        ((4, "H"), [(4, "S"), (9, "S"), (1, "C"), (4, "D")], 6, "simple, triple 4 "),
+        ((4, "H"), [(4, "S"), (9, "S"), (4, "C"), (4, "D")], 12, "simple, quadruple 4 ")
     ])
-def test_scores(starter, hand,   expected_score, description):
+def test_multiples(starter, hand, expected_score, description):
 
-        calculated_score = cribbage_scorer.score(starter, hand)
-        assert cribbage_scorer.score(starter, hand) == expected_score, \
-            f"The calculated score was: {calculated_score}, the expected score: {expected_score}. " + \
-            f"The starter card was: {starter} and hand was: {hand}" + \
-            f"The hand description was: {description}"
+    calculated_score = cribbage_scorer.score(starter, hand)
+    assert calculated_score[0] == expected_score, \
+        f"The calculated score was: {calculated_score}, the expected score: {expected_score}. " + \
+        f"The starter card was: {starter} and hand was: {hand}" + \
+        f"The hand description was: {description}"
 
+
+@pytest.mark.parametrize("starter, hand, expected_score, description", [
+        ((1, "S"), [(5, "D"), (10, "S"), (2, "C"), (6, "H")], 2, "simple, 15,= from 1 x 2 cards"),
+        ((2, "S"), [(5, "D"), (10, "S"), (9, "C"), (6, "H")], 4, "simple, 2 x 15 from 2 x 2 cards"),
+        ((2, "S"), [(3, "D"), (10, "S"), (7, "C"), (9, "H")], 2, "simple, 1 x 15 from 1 x 3 cards"),
+        ((2, "S"), [(3, "D"), (5, "S"), (6, "C"), (8, "H")], 2, "simple, 1 x 15 from 1 x 3 cards")
+])
+def test_fifteens(starter, hand, expected_score, description):
+
+    calculated_score = cribbage_scorer.score(starter, hand)
+    assert calculated_score[0] == expected_score, \
+        f"The calculated score was: {calculated_score}, the expected score: {expected_score}. " + \
+        f"The starter card was: {starter} and hand was: {hand}" + \
+        f"The hand description was: {description}"
+
+# ((11,"S"), [(12,"D"),(13,"S"), (1,"C"), (9,"H")], 3, "simple, 3 card run"),
+# ((5,"H"), [(5,"S"),(4,"S"), (2,"S"), (6,"H")], 12, "mixed, 15s, double run"),
+# ((5,"H"), [(6,"D"),(11,"H"), (4,"H"), (7,"C")], 9, "mixed, 15s, run, nob"),
+# ((5,"H"), [(10,"S"),(8,"D"), (13,"C"), (8,"C")], 6, "mixed, 15s, pair")
