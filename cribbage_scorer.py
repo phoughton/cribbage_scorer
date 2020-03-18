d3853563260e2aa1090f93e19f1e1bc9cad0e98d
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-03-18 13:21:44 +0000

    Remove redundant code for tuple handling

--- cribbage_scorer.py
+++ cribbage_scorer.py
@@ -6,19 +6,27 @@ def score(starter, hand):
     running_score = 0
     running_msg = ""
 
-    multiples_score, multiples_msg = multiples(starter, hand)
+    card_nums = card_numbers(starter, hand)
+    multiples_score, multiples_msg = multiples(card_nums)
     running_score += multiples_score
     running_msg = running_msg + multiples_msg
 
-    fifteens_score, fifteens_msg = fifteens(starter, hand)
+    fifteens_score, fifteens_msg = fifteens(card_nums)
     running_score += fifteens_score
     running_msg = running_msg + fifteens_msg
 
-    runs_score, runs_msg = runs(starter, hand)
+    runs_score, runs_msg = runs(card_nums)
     running_score += runs_score
     running_msg = running_msg + runs_msg
 
     return running_score, running_msg
+
+
+def card_numbers(starter, hand):
+    print("Combined hand: " + str(hand + [starter]))
+    card_nums = extract_numbers(hand + [starter])
+    card_nums.sort()
+    return card_nums
 
 
 def extract_numbers(card_tuples):
@@ -29,11 +37,7 @@ def extract_numbers(card_tuples):
     return ranks
 
 
-def runs(starter, hand):
-    all_cards = hand + [starter]
-    print("Combined hand: " + str(all_cards))
-    card_nums = extract_numbers(all_cards)
-    card_nums.sort()
+def runs(card_nums):
 
     running_score = 0
     running_msg = ""
@@ -46,29 +50,29 @@ def runs(starter, hand):
     return running_score, running_msg
 
 
-def runs_reg(starter, hand):
-    all_cards = hand + [starter]
-    print("Combined hand: " + str(all_cards))
-
-    running_msg = ""
-    all_cards.sort()
-    current_run_length = 0
-    last_card = None
-    for card in all_cards:
-        if current_run_length == 0:
-            current_run_length += 1
-        else:
-            if card[0] == last_card[0] + 1:
-                current_run_length += 1
-            else:
-                current_run_length = 1
-
-        last_card = card
-
-    if current_run_length >= 3:
-        return current_run_length, running_msg
-    else:
-        return 0, ""
+# def runs_reg(starter, hand):
+#     all_cards = hand + [starter]
+#     print("Combined hand: " + str(all_cards))
+#
+#     running_msg = ""
+#     all_cards.sort()
+#     current_run_length = 0
+#     last_card = None
+#     for card in all_cards:
+#         if current_run_length == 0:
+#             current_run_length += 1
+#         else:
+#             if card[0] == last_card[0] + 1:
+#                 current_run_length += 1
+#             else:
+#                 current_run_length = 1
+#
+#         last_card = card
+#
+#     if current_run_length >= 3:
+#         return current_run_length, running_msg
+#     else:
+#         return 0, ""
 
 
 def face_valuer(raw_card_value):
@@ -78,56 +82,52 @@ def face_valuer(raw_card_value):
         return 10
 
 
-def fifteens(starter, hand):
-    all_cards = hand + [starter]
-    print("Combined hand: " + str(all_cards))
+def fifteens(card_nums):
 
     running_score = 0
     score_msg = ""
 
-    combinations_2_cards = itertools.combinations(all_cards, 2)
+    combinations_2_cards = itertools.combinations(card_nums, 2)
     for two_card_seq in combinations_2_cards:
-        if face_valuer(two_card_seq[0][0]) + face_valuer(two_card_seq[1][0]) == 15:
+        if face_valuer(two_card_seq[0]) + face_valuer(two_card_seq[1]) == 15:
             running_score += 2
             score_msg = score_msg + f"15 from {two_card_seq}"
 
-    combinations_3_cards = itertools.combinations(all_cards, 3)
+    combinations_3_cards = itertools.combinations(card_nums, 3)
     for three_card_seq in combinations_3_cards:
-        if face_valuer(three_card_seq[0][0]) + \
-                face_valuer(three_card_seq[1][0]) + \
-                face_valuer(three_card_seq[2][0]) == 15:
+        if face_valuer(three_card_seq[0]) + \
+                face_valuer(three_card_seq[1]) + \
+                face_valuer(three_card_seq[2]) == 15:
             running_score += 2
             score_msg = score_msg + f"15 from {three_card_seq}"
 
-    combinations_4_cards = itertools.combinations(all_cards, 4)
+    combinations_4_cards = itertools.combinations(card_nums, 4)
     for four_card_seq in combinations_4_cards:
-        if face_valuer(four_card_seq[0][0]) + \
-                face_valuer(four_card_seq[1][0]) + \
-                face_valuer(four_card_seq[2][0]) + \
-                face_valuer(four_card_seq[3][0]) == 15:
+        if face_valuer(four_card_seq[0]) + \
+                face_valuer(four_card_seq[1]) + \
+                face_valuer(four_card_seq[2]) + \
+                face_valuer(four_card_seq[3]) == 15:
             running_score += 2
             score_msg = score_msg + f"15 from {four_card_seq}"
 
-    combinations_5_cards = itertools.combinations(all_cards, 5)
+    combinations_5_cards = itertools.combinations(card_nums, 5)
     for five_card_seq in combinations_5_cards:
-        if face_valuer(five_card_seq[0][0]) + \
-                face_valuer(five_card_seq[1][0]) + \
-                face_valuer(five_card_seq[2][0]) + \
-                face_valuer(five_card_seq[3][0]) + \
-                face_valuer(five_card_seq[4][0]) == 15:
+        if face_valuer(five_card_seq[0]) + \
+                face_valuer(five_card_seq[1]) + \
+                face_valuer(five_card_seq[2]) + \
+                face_valuer(five_card_seq[3]) + \
+                face_valuer(five_card_seq[4]) == 15:
             running_score += 2
             score_msg = score_msg + f"15 from {five_card_seq}"
 
     return running_score, score_msg
 
 
-def multiples(starter, hand):
-    all_cards = hand + [starter]
-    print("Combined hand: " + str(all_cards))
+def multiples(card_nums):
 
     multiples_counter = defaultdict(lambda: 0)
-    for card in all_cards:
-        multiples_counter[card[0]] += 1
+    for card in card_nums:
+        multiples_counter[card] += 1
 
     multiples_found = {}
     for card_name, count in multiples_counter.items():
