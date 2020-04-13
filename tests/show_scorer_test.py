efda153ef97c13fc7e030e33479ecff595f79dde
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-04-13 13:11:02 +0100

    restructure tests, split out impossible test from others.

--- tests/show_scorer_test.py
+++ tests/show_scorer_test.py
@@ -141,27 +141,3 @@ def test_etc_hands__crib_affected(starter, hand, crib, expected_score, descripti
         f"The starter card was: {starter} and hand was: {hand}, " + \
         f"The hand description was: {description} "
 
-
-@pytest.fixture(params=range(0, 1000))
-def card_sets(request):
-        suits = ['H', 'C', 'S', 'D']
-        ranks = range(1, 13)
-        cards = set()
-        while len(cards) < 5:
-            suit = random.choice(suits)
-            rank = random.choice(ranks)
-            cards.add((rank, suit))
-        return list(cards)
-
-
-def test_for_impossible_scores(card_sets):
-
-    starter_card = card_sets.pop()
-    hand = card_sets
-
-    calculated_score = cribbage_scorer.show_calc_score(starter_card, hand)
-
-    assert calculated_score[0] not in [19, 25, 26, 27], \
-        f"The calculated score was: {calculated_score}, this was an impossible score " + \
-        f"The starter card was: {starter_card} and hand was: {hand}, "
-
