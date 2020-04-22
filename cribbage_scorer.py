bc35a3366fae15384b5a51114862ae0d86f4398e
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-04-22 17:03:11 +0100

    fix pep8 issue import on same line

--- cribbage_scorer.py
+++ cribbage_scorer.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-import itertools, more_itertools
+import itertools
+import more_itertools
 
 card_names = {1: "Ace", 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: "Jack", 12: "Queen", 13: "King"}
 
