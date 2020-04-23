c05a7c0bd629dbe864f7bbced79ff946772c4d4e
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-04-23 11:34:07 +0100

    add explicit requirements.txt for more itertools

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cribbage_scorer",
-    version="0.0.4",
+    version="0.0.5",
     author="Peter Houghton",
     author_email="pete@investigatingsoftware.co.uk",
     description="A Cribbage scoring tool",
